75af26d28b8c68e961732cd3d12f27eb59f47894
oddy <jerony_cruz@ucne.edu.do>
2026-02-05 22:51:08 -0400

    Feat(EP): add functions in interface to use in service impl.

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,11 +1,15 @@
 package com.example.demo.services;
 
-import com.example.demo.dto.usuarioDto.UserRequestDTO;
-import com.example.demo.dto.usuarioDto.UserResponseDTO;
+import com.example.demo.dto.userDto.UserRequestDTO;
+import com.example.demo.dto.userDto.UserResponseDTO;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface UserService {
     UserResponseDTO insertUser(UserRequestDTO dto);
     List<UserResponseDTO> listUser();
+    UserResponseDTO getUserById(int id);
+    UserResponseDTO updateUser(int id, UserRequestDTO dto);
+    void deleteUser(int id);
 }
