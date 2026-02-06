a70f6b89281e6e6030d98135f1176d01206eb4f0
oddy <jerony_cruz@ucne.edu.do>
2026-02-05 22:50:05 -0400

    Feat(EP): add new Endpoints to get by id, delete and edit..

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.usuarioDto.UserRequestDTO;
-import com.example.demo.dto.usuarioDto.UserResponseDTO;
+import com.example.demo.dto.userDto.UserRequestDTO;
+import com.example.demo.dto.userDto.UserResponseDTO;
 import com.example.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/usuarios")
@@ -26,5 +27,22 @@ public class UserController {
     @GetMapping
     public List<UserResponseDTO> getUsers(){
         return userService.listUser();
+    }
+
+    @GetMapping("/{id}")
+    public UserResponseDTO getUserById(@PathVariable int id){
+        return userService.getUserById(id);
+    }
+
+    @PutMapping("/{id}")
+    public UserResponseDTO updateUser (@PathVariable int id,
+                                       @RequestBody UserRequestDTO dto) {
+        return userService.updateUser(id,dto);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable int id){
+        userService.deleteUser(id);
     }
 }
