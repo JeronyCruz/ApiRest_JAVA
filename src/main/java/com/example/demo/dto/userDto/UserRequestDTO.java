50e715220d992a3f29ca0c77dd57847e4c54dce8
oddy <jerony_cruz@ucne.edu.do>
2026-02-06 22:14:47 -0400

    Fix(): validations for atributes

--- src/main/java/com/example/demo/dto/userDto/UserRequestDTO.java
+++ src/main/java/com/example/demo/dto/userDto/UserRequestDTO.java
@@ -1,12 +1,22 @@
 package com.example.demo.dto.userDto;
 
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserRequestDTO {
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(min = 3, max = 50, message = "El nombre debe tener entre 3 y 50 caracteres")
     private String nombre;
+
+    @NotBlank(message = "El nombre es obligatorio")
+    @Email(message = "Formato de email invalido")
     private String email;
+
+    @NotNull(message = "La edad es obligatoria")
+    @Min(value = 18, message = "La edad minima es 18")
+    @Max(value = 120, message = "La edad maxima es 120")
     private int edad;
 }
