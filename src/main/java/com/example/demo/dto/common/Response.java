822962a0007411b88134e0400d8e310f0a88c0fe
oddy <jerony_cruz@ucne.edu.do>
2026-02-06 22:14:07 -0400

    Feat(Exeptions): add Response class

---
+++ src/main/java/com/example/demo/dto/common/Response.java
@@ -0,0 +1,18 @@
+package com.example.demo.dto.common;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Response<T> {
+    private boolean success;
+    private String message;
+    private T data;
+
+    public Response(boolean success, String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+}
